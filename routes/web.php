9da14c4785d27e8fbe119692595df68ba8eb9c6b
IyaTulus <75067698+littleboy12@users.noreply.github.com>
2025-01-11 13:50:02 +0700

    Feature : FE Login and Register

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/auth', 'template.auth');
+Route::view('/auth-login', 'auth.login');
+Route::view('/auth-register', 'auth.register');
