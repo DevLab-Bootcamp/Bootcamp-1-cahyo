7d23591f2ab09356f8140f2408c2d29d8db410d1
IyaTulus <75067698+littleboy12@users.noreply.github.com>
2025-01-12 20:51:07 +0700

    add sidebar template

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('template.main')
+@section('title-page', "Dashboard")
+@section('main-content')
+    <h1>ini main</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+Route::view('/', 'dashboard');
 Route::view('/auth', 'template.auth');
+Route::view('/main', 'template.main');
 Route::view('/auth-login', 'auth.login');
 Route::view('/auth-register', 'auth.register');
